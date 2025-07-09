4660f3f7b02818daedbb690b3f601c4531ff56c6
bertinmick69 <bertinmick@yahoo.fr>
2025-07-09 16:35:48 +0200

    job 01

---
+++ jour01/index.php
@@ -0,0 +1,17 @@
+
+<?php
+$str = "LaPlateforme ";
+$str2 = "vivre";
+$str3 = "!";
+
+echo "  $str $str2 $str3 ";
+
+echo "<script> window.alert('bonjour') </script>";  
+
+$val = 6+4; 
+echo " $val ";
+
+$myBool=true;  //  false;
+echo "$myBool ";
+
+?>
