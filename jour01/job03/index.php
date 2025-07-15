b4ca05114a687671b66f49c88590269587a6c332
bertinmick69 <bertinmick@yahoo.fr>
2025-07-15 14:11:59 +0200

    finish job03

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -1,38 +1,66 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
+<?php
 $boolean=true;
 $entier=11;
-$chaineDeCaractere="chaine de caractere";
+$chaineDeCaractere="hello moi ";
 $flottant=10.5;
-
 echo "$boolean $entier $chaineDeCaractere $flottant";
 
 ?>
 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+
+
   <style>
-    tr,td{border: black solid 2px;
+   tr,td{border: black solid 2px;
         text-align: center;
     }
+    
+    thead{ border: black solid 2px;
+      text-align: center;
+    background-color: yellowgreen;}
   </style> 
+
+
 <table>
-       <tr>
-            <td>type</td>
-            <td>nom</td>
-            <td>valeur</td>
+       <thead> 
+        <tr> 
+            <th>type</th>
+            <th>nom</th>
+            <th>valeur</th>
+          </tr>
+        </thead>
+        <tbody> 
+         <tr>
+            <td><?php echo gettype($boolean);?></td>
+            <td><?php echo " boolean" ;?></td>
+            <td><?php echo "$boolean";?></td>
          </tr>
          <tr>
-            <td><?php echo gettype($boolean)?></td>
-            <td><?php echo"$chaineDeCaractere"?></td>
-            <td><?php echo"$flottant"?></td>
+            <td><?php  echo gettype($chaineDeCaractere); ?></td>
+            <td><?php  echo"chaineDeCaractere";?></td>
+            <td><?php echo"$chaineDeCaractere";?></td>
          </tr>
+         <tr>
+            <td><?php echo gettype($flottant); ?></td>
+            <td><?php echo" flottant";?></td>
+            <td><?php echo"$flottant";?></td>
+         </tr>
+         <tr>
+            <td><?php echo gettype($entier);?></td>
+            <td><?php echo"entier";?></td>
+             <td><?php echo $entier; ?></td>
+         </tr>
+       </tbody>
 
   </table>
         
