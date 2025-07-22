a245366ef4a306ce05a98734e1409617982f7ebc
bertinmick69 <bertinmick@yahoo.fr>
2025-07-22 15:13:01 +0200

    job04 finish

--- jour02/job04/index.php
+++ jour02/job04/index.php
@@ -3,24 +3,18 @@
 
 for ($i = 1; $i <= 100; $i++) {
 
-    // ● Si le nombre est un multiple de 3, affichez “Fizz”.
+    // ● Si le nombre est un multiple de 3, et pas un mutiple de 5 affichez “Fizz”.
     if ($i % 3 == 0 && $i % 5 != 0) {
         echo "FIZZ <br>";
     } elseif ($i % 5 == 0 && $i % 3 != 0)
-    // ● Si le nombre est un multiple de 5, affichez “Buzz”.
+    // ● Si le nombre est un multiple de 5, et pas un mutiple de 3 affichez “Buzz”.
     {
         echo "BUZZ <br>";
     } elseif ($i % 3 == 0 || $i % 5 == 0) {
+        // ● Si le nombre est un multiple de 3 et de 5, affichez “FizzBuzz”.
         echo "FizzBuzz <br>";
     } else {
+        // alor tu maffiche tout les nombre de 1 a 100
         echo "$i <br>";
     }
-
-    // ● Si le nombre est un multiple de 3 et de 5, affichez “FizzBuzz”.
-
-
-
-
-
-
 }
