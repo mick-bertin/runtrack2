b468949939372ce755ed53a9c1aa128aa9c88a85
bertinmick69 <bertinmick@yahoo.fr>
2025-07-26 21:08:14 +0200

    job06 finish

---
+++ jour02/job06/index.php
@@ -0,0 +1,38 @@
+<!-- Faire un algorithme qui affiche un rectangle de 20 de largeur par 10 de hauteur. Ces
+dimensions devront être stockées dans deux variables $largeur et $hauteur, modifiables
+facilement. -->
+<!-- pour une largeur -->
+
+<?php
+// nome une hauteur et une largeur 
+// parcour largeur
+// Parcours hauteur
+///////Affiche largeur et la hauteur 
+//////si j affiche largeur et la hauteur 
+/////////sinon je remplis avec du vide 
+
+///////affiche la hauteur et fait un saut de ligne echo "   ";
+
+
+$largeur = 20;
+$hauteur = 10;
+
+
+
+
+for ($i = 0; $i <= $largeur; $i++) {
+    // echo $i; 
+    echo "<br>";
+    for ($h = 1; $h <= $hauteur; $h++) {
+        echo $h;
+
+        if ($i == 0 || $i == $largeur  || $h == 1 || $h == $hauteur) {
+            echo ".";
+            echo "\n";
+        } else {
+
+            echo "\n";
+            echo " ";
+        }
+    }
+}
