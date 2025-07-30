1e9a6f0be29e422408ae83a12916caf3564ded31
bertinmick69 <bertinmick@yahoo.fr>
2025-07-30 14:45:47 +0200

    jour03- job04-finish

---
+++ jour03/job04/index.php
@@ -0,0 +1,15 @@
+<!-- Créez une variable de type string nommée $str et affectez y le texte :
+“Dans l'espace, personne ne vous entend crier”.
+Créez un algorithme qui parcourt, compte et affiche le nombre de caractères présents
+dans cette chaîne. -->
+
+
+<?php
+$str = "Dans l'espace, personne ne vous entend crier";
+
+for ($i = 0; isset($str[$i]); $i++) {
+}
+
+echo "il y a $i nombre de caractères présents";
+
+?>
